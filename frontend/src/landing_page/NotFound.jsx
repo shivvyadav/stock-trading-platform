3827642b066bc821efc326f318a4477e7720c0bc
Shiv  Yadav <shivyadav0014@gmail.com>
2026-02-16 20:21:22 +0545

    add global not found component

---
+++ frontend/src/landing_page/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6 text-center">
+      <h1 className="text-xl sm:text-3xl font-bold">4️⃣0️⃣4️⃣ - Not Found</h1>
+      <p className="mt-4 text-sm sm:text-xl">
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
